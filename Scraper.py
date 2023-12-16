8965b54b437e74668c80c7bc15199f2fcfdbf3db
Sanjay <sanjaynandwani2027@gmail.com>
2023-12-16 13:10:43 +0530

    extracted top 10 videos. implemented smtp for sending mails with attachments (.csv file)

--- Scraper.py
+++ Scraper.py
@@ -1,7 +1,18 @@
+from email import encoders
+from email.mime.base import MIMEBase
+
+import body as body
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import pandas as pd
+import smtplib
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import json
+
+
 
 YouTube_trending_URL = 'https://www.youtube.com/feed/trending'
 
@@ -47,8 +58,51 @@ def parse_video(video):
         "time_uploaded ": time_uploaded
     }
 
+def send_email(body):
+    try:
+        server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server_ssl.ehlo()  # optional
+
+        Sender_email = os.environ.get('sender_email')
+        Receiver_email = "pateljaisvi7@gmail.com"
+        Sender_password = os.environ.get('gmail_password')
+
+        subject = 'YouTube trending videos'
+
+        # Create the MIMEMultipart object
+        msg = MIMEMultipart()
+
+        # Attach the subject directly to the msg object
+        msg['From'] = Sender_email
+        msg['To'] = Receiver_email
+        msg['Subject'] = subject
+
+        # Attach the body of the email
+        msg.attach(MIMEText(body, 'plain'))
+
+        # Specify the filename to attach from the project directory
+        filename = 'trending.csv'
+        # Get the absolute path to the file
+        file_path = os.path.abspath(filename)
+        # Attach the file
+        attachment = open(file_path, 'rb')
+
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', f'attachment; filename="{filename}"')
+        msg.attach(part)
+
+        server_ssl.login(Sender_email, Sender_password)
+        server_ssl.sendmail(Sender_email,Receiver_email,msg.as_string())
+        server_ssl.close()
+
+    except Exception as e:
+        print(f'Something went wrong: {e}')
+
 
 if __name__ == "__main__":
+
     print("creating the driver:")
     driver = get_driver()
 
@@ -64,8 +118,13 @@ if __name__ == "__main__":
 
     print("save the data to a dataframe: ")
     videos_df = pd.DataFrame(videos_data)
-    print(videos_df)
+    #print(videos_df)
     videos_df.to_csv('trending.csv', index=None)
+
+    print("send an email with results")
+    body = json.dumps(videos_data,indent=3)
+    send_email(body)
+
 
 
 
